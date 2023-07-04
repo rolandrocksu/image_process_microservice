0cfcafca88e13772b02a51db615c6df99af2677e
roland <roland.poghosyan@gohacktech.com>
2023-07-04 12:18:13 +0400

    Renamed microservice

--- image_process_microservice/asgi.py
+++ image_process_microservice/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for image_resize_microservice project.
+ASGI config for image_process_microservice project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_resize_microservice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_process_microservice.settings')
 
 application = get_asgi_application()
--- image_process_microservice/wsgi.py
+++ image_process_microservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for image_resize_microservice project.
+WSGI config for image_process_microservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_resize_microservice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_process_microservice.settings')
 
 application = get_wsgi_application()
